4841c828c4d106459cbc0bb5c017b9470f15e99a
nhut <nhut.nguyen@toancausoft.com.vn>
2014-09-17 16:11:31 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicAuth::Application.config.secret_token = '2211d4bc0b724c4090a1bca7f0de30848e37a0c6c85701bc1bd27e4acf4f18c3bf605d701a1165f075fe704220365d19f06d70ee252ebb4664f6451d62f0969d'
